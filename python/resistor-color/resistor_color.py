37ecff32a51f5ace903e0f8f38237f107b08e9b0
Shik0 <mikailovsh@gmail.com>
2023-06-08 22:04:43 +0200

    Accepted solution

---
+++ python/resistor-color/resistor_color.py
@@ -0,0 +1,22 @@
+def color_code(color):
+    
+    color_names = colors()
+
+    return color_names.index(color)
+
+def colors():
+    
+    color_array = [
+        "black",
+        "brown",
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "violet",
+        "grey",
+        "white"
+    ]
+
+    return color_array
