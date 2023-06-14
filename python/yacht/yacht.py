bb61ed5165fbb99b1983b35c3e6af22f1ab9bca2
Shik0 <mikailovsh@gmail.com>
2023-06-14 21:09:30 +0200

    Task: Yacht

---
+++ python/yacht/yacht.py
@@ -0,0 +1,18 @@
+# Score categories.
+# Change the values as you see fit.
+YACHT = 50
+ONES = 1
+TWOS = 2
+THREES = 3
+FOURS = 4
+FIVES = 5
+SIXES = 6
+FULL_HOUSE = 'all_dice_sum'
+FOUR_OF_A_KIND = 'four_dice_sum'
+LITTLE_STRAIGHT = 30
+BIG_STRAIGHT = 30
+CHOICE = 'choice'
+
+
+def score(dice, category):
+    pass
