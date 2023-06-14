eb81674312bae715a313d58db3c16bb4573889bf
Shik0 <mikailovsh@gmail.com>
2023-06-14 21:23:13 +0200

    Add conditions

--- python/yacht/yacht.py
+++ python/yacht/yacht.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 # Score categories.
 # Change the values as you see fit.
 YACHT = 50
@@ -7,12 +9,21 @@ THREES = 3
 FOURS = 4
 FIVES = 5
 SIXES = 6
-FULL_HOUSE = 'all_dice_sum'
-FOUR_OF_A_KIND = 'four_dice_sum'
+FULL_HOUSE = 'full_hause'
+FOUR_OF_A_KIND = 'four_of_a_kind'
 LITTLE_STRAIGHT = 30
 BIG_STRAIGHT = 30
 CHOICE = 'choice'
 
 
 def score(dice, category):
-    pass
+    
+    if type(category) is int and category < 30:
+        return sum([d for d in dice if d == category])
+    
+    if category == 'full_hause' and len(Counter(dice) == 2):
+        return sum(dice)
+    
+    if category == 'four_of_a_kind' and 
+
+    return None
