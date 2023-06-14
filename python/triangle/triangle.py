bb2b66c84cb549d1c2b3ec476bcd1f80a7b790b4
Shik0 <mikailovsh@gmail.com>
2023-06-14 20:34:20 +0200

    Accepted solution

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -12,19 +12,24 @@ def isosceles(sides):
 
     zero = 0
 
-    if zero in sides or side_length(sides):
-        return False    
+    if zero in sides or not side_length(sides):
+        return False   
     
     return (sides[0]==sides[1]) or (sides[0]==sides[2]) or (sides[1]==sides[2])
 
 
 def scalene(sides):
-    pass
+
+    zero = 0
+
+    if zero in sides or not side_length(sides):
+        return False
+
+    return (sides[0]!=sides[1]) and (sides[0]!=sides[2]) and (sides[1]!=sides[2])
 
 
 def side_length(sides):
 
-    
-    return True if (sides[0]+sides[1]>=sides[2]) or \
-        (sides[0]+sides[2]>=sides[1]) or \
+    return True if (sides[0]+sides[1]>=sides[2]) and \
+        (sides[0]+sides[2]>=sides[1]) and \
             (sides[2]+sides[1]>=sides[0]) else False
