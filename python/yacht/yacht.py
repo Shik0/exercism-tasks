0a79433890a82d3a713c197b5a494e65c620d39c
Shik0 <mikailovsh@gmail.com>
2023-06-19 00:59:21 +0200

    Accepted solution

--- python/yacht/yacht.py
+++ python/yacht/yacht.py
@@ -2,7 +2,7 @@ from collections import Counter
 
 # Score categories.
 # Change the values as you see fit.
-YACHT = 50
+YACHT = 'yacht'
 ONES = 1
 TWOS = 2
 THREES = 3
@@ -11,19 +11,43 @@ FIVES = 5
 SIXES = 6
 FULL_HOUSE = 'full_hause'
 FOUR_OF_A_KIND = 'four_of_a_kind'
-LITTLE_STRAIGHT = 30
-BIG_STRAIGHT = 30
+LITTLE_STRAIGHT = 'little_straight'
+BIG_STRAIGHT = 'big_straight'
 CHOICE = 'choice'
 
 
 def score(dice, category):
     
+    zero = 0
+
+    # ones - sixes
     if type(category) is int and category < 30:
         return sum([d for d in dice if d == category])
     
-    if category == 'full_hause' and len(Counter(dice) == 2):
+    # full house
+    if category == 'full_hause' and 3 in Counter(dice).values():
         return sum(dice)
     
-    if category == 'four_of_a_kind' and 
+    # four of a kind
+    if  category == 'four_of_a_kind':
+        for key,value in Counter(dice).items():
+            if value >= 4:
+                return 4 * key
+            
+    # big straight
+    if category == 'big_straight' and sum(dice) == 20:
+        return 30
+    
+    # little straight
+    if category == 'little_straight' and sum(dice) == 15:
+        return 30
+    
+    # choice
+    if category == 'choice':
+        return sum(dice)
+    
+    # yacht
+    if category == 'yacht' and len(Counter(dice)) == 1:
+        return 50
 
-    return None
+    return zero
